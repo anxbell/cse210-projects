494a78006a7f2d28303401f0ccf95d62203f14d1
anxbell <valerieanabell@gmail.com>
2024-10-02 23:49:33 -0600

    resume activity added

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+
+//class
+        Job job1 = new Job(); //Create a new job instance named job1 .
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job(); //Create a new job instance named job1 .
+
+        job2._jobTitle = "Front-end Developer";
+        job2._company = "Microsoft";
+        job2._startYear = 2010;
+        job2._endYear = 2020;
+
+
+        Resume myResume = new Resume();//new instance of the Resume class.
+        myResume._name = "Valerie Sanchez";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayJobDetails();
+
+
     }
 }
