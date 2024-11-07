ef4905195c2661c4c1ebaa0521d4b618a92c23bc
anxbell <valerieanabell@gmail.com>
2024-11-07 12:16:46 -0600

    Assignment added

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,27 +1,27 @@
 using System;
 
-class Word //this class hides words replacing them for underscores based on the length of the word.
-{ //responsible for managing its own state (hidden or shown).
-    private string _text; //Attributes
+class Word
+{ 
+    private string _text; 
     private bool _wordHidden;
 
-    public Word(string text) //Constructor
+    public Word(string text) 
     {
         _text = text;
         _wordHidden = false;
     }
 
-    public void Hide() //method
+    public void Hide()
     {
         _wordHidden = true;
     }
 
-    public bool IsWordHidden() //Getter Method
+    public bool IsWordHidden() 
     {
         return _wordHidden;
     }
 
-    public string GetText() //method
+    public string GetText() 
     {
         string blankSpace = new string('_', _text.Length);
 
